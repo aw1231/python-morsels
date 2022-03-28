24e81d4e4856794896bc248f356e7d46e737070b
Alan Williams <astropiloto@gmail.com>
2022-03-28 11:09:09 -0500

    fix linter round 2

--- count.py
+++ count.py
@@ -1,9 +1,9 @@
 from typing import Dict
 
 
-def count_words(input: str) -> Dict[str,int]:
+def count_words(input: str) -> Dict[str, int]:
     in_split = input.split()
-    dict = {}
+    dict: Dict[str, int] = {}
     for word in in_split:
         word_normalized = word.lower()
         if word_normalized in dict:
